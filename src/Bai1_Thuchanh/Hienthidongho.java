5387850c0dd3caa41d7184e63a18a16088a3c164
bachtran0330 <0932085552a@gmail.com>
2021-12-06 15:42:30 +0700

    push bai tap ngay 6/12/2021

---
+++ src/Bai1_Thuchanh/Hienthidongho.java
@@ -0,0 +1,10 @@
+package Bai1_Thuchanh;
+
+import java.util.Date;
+
+public class Hienthidongho {
+    public static void main(String[] args) {
+        Date now = new Date();
+        System.out.println("date now:"+ now);
+    }
+}
